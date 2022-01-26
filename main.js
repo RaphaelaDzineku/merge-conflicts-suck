007852be29cab0f19b07964839ed061494f5999d
Dzineku <Raphaela.Dzineku@computacenter.com>
2022-01-26 09:37:50 +0100

    back to the beginning

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 var num = 50;
-<<<<<<< HEAD
-var truth = "Hello Flatter World";
-=======
-var truth = "Hello Flat World";
->>>>>>> 1b2a1f003d301a087559b73849d7366e2f85d4b1
+var truth = "Hello World";
